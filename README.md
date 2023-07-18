cb5aaf51c228b4d4b8d6a031cbd3ccfe8bec8ef9
Will Douglas <will_djm@hotmail.com>
2023-07-18 14:23:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1 align="center"> Buscador de CEP </h1>
+<br>
+
+<p align="center">
+  <img alt="projeto buscador de ceps" src=".github\preview.jpg" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- ReactJS
+- CSS
+- Git e Github
+
+## 💻 Projeto
+
+O DevLinks é um agregador de links para usar como cartão de visitas online.
+
+- [Acesse o projeto finalizado, online](https://willdjm.github.io/buscadorcep)
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT.
+
+---
---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import "./styles.css";
+import api from "./services/api";
+
+function App() {
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+
+  async function handleSearch(){
+    if(input === ''){
+      alert("Preencha algum cep!")
+      return;
+    } 
+    try{
+      const response = await api.get(`${input}/json`)
+      setCep(response.data);
+      setInput("")
+
+
+    }catch{
+      alert("Erro");
+      setInput("")
+
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+      <div className="containerInput">
+        <input 
+          type="text" 
+          placeholder="Digite seu cep..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#fff" />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 &&(
+        <main className="main">
+        <h2>CEP: {cep.cep}</h2>
+        <span>Logradouro: {cep.logradouro}</span>
+        <span>Complemento: {cep.complemento}</span>
+        <span>Bairro: {cep.bairro}</span>
+        <span>Cidade: {cep.localidade} - {cep.uf}</span>
+      </main>
+      )}
+    
+    </div>
+    
+  );
+}
+
+export default App;
